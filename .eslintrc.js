e755c55801f236f0ac83da685032e0d4f0c9afae
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-05-15 17:11:00 +0100

    feat: add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,13 @@ module.exports = {
     node: true
   },
   extends: [
-    // By extending from a plugin config, we can get recommended rules without having to add them manually.
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:import/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:@typescript-eslint/recommended",
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    "eslint-config-prettier"
+    "eslint-config-prettier",
+    "plugin:storybook/recommended"
   ],
   settings: {
     react: {
